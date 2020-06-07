1d978925756a1c9dd60019e1e38e46c109ee755e
Nishant Shah <shah.nishant19@gmail.com>
2020-06-07 18:33:10 +0530

    Show time taken

--- app/src/main/java/shah/nishant/findingfalcone/game/view/TargetViewHolder.kt
+++ app/src/main/java/shah/nishant/findingfalcone/game/view/TargetViewHolder.kt
@@ -4,6 +4,9 @@ import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import shah.nishant.findingfalcone.R
 import shah.nishant.findingfalcone.databinding.TargetLayoutBinding
+import shah.nishant.findingfalcone.extensions.gone
+import shah.nishant.findingfalcone.extensions.invisible
+import shah.nishant.findingfalcone.extensions.visible
 import shah.nishant.findingfalcone.game.model.Target
 
 class TargetViewHolder constructor(
@@ -23,18 +26,25 @@ class TargetViewHolder constructor(
     fun bind(target: Target) {
         binding.apply {
             name.text = target.planet.name
-            distance.text = distance.context.getString(R.string.distance, target.planet.distance)
+            distance.text = distance.context.getString(R.string.distance, target.planet.distance.toString())
             photo.setImageResource(PLANET_DRAWABLES[adapterPosition % PLANET_DRAWABLES.size])
             if (target.vehicle == null) {
                 selectVehicle.setText(R.string.select_vehicle)
                 selectVehicle.setTextColor(selectVehicle.context.resources.getColor(R.color.black))
                 selectVehicle.backgroundTintList =
                     ContextCompat.getColorStateList(selectVehicle.context, R.color.white)
+                timeTaken.invisible()
             } else {
                 selectVehicle.text = target.vehicle.name
                 selectVehicle.setTextColor(selectVehicle.context.resources.getColor(R.color.white))
                 selectVehicle.backgroundTintList =
                     ContextCompat.getColorStateList(selectVehicle.context, R.color.green)
+                timeTaken.text = timeTaken.context.getString(
+                    R.string.time_taken,
+                    // Hope API would never respond with vehicle.distance = 0
+                    (target.planet.distance!! / target.vehicle.speed!!).toString()
+                )
+                timeTaken.visible()
             }
             selectVehicle.setOnClickListener { selectVehicle(target) }
         }
